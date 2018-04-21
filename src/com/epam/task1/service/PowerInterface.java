f4605c57751acc99baeb8d34d72ad3f818926a88
aliaksandr-harkusha <38460516+aliaksandr-harkusha@users.noreply.github.com>
2018-04-21 16:47:48 +0300

    Update PowerInterface.java

--- src/com/epam/task1/service/PowerInterface.java
+++ src/com/epam/task1/service/PowerInterface.java
@@ -1,5 +1,7 @@
 package com.epam.task1.service;
 
+//Если это interface то его имя не должно содержать слово interface
+//подумай как его лучше назвать
 public interface PowerInterface {
 	void powerOn();
 	
